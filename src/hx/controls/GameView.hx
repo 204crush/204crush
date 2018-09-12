57cc89be73e42f82e82c92fae7158fd215d70251
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2018-09-12 11:10:20 +0300

    Target rect. font added.

--- src/hx/controls/GameView.hx
+++ src/hx/controls/GameView.hx
@@ -49,11 +49,11 @@ class GameView extends Container
 		
 		this.score = new Score();
 		this.score.x = 640;
-		this.score.y = 110;
+		this.score.y = 90;
 		
 		this.praises = new PraiseManager();
 		this.praises.x = control.x + Math.floor(( GridControl.BLOCK_WIDTH * GridLogic.GRID_WIDTH ) / 2);
-		this.praises.y = control.y + Math.floor(( GridControl.BLOCK_WIDTH * GridLogic.GRID_WIDTH ) / 2);
+		this.praises.y = 100;
 		
 		this.addChild(this.bg);
 		this.addChild(this.control);
@@ -82,17 +82,33 @@ class GameView extends Container
 	public function resize(size:Rectangle):Void
 	{
 		this.size = size;
+		var tr:Rectangle = this.getTargetRect();
+		this.width = tr.width;
+		this.height = tr.height;
+		this.x = tr.x;
+		this.y = tr.y;
+	}
+	
+	private var minRectGame:Rectangle = new Rectangle(272, 50, 1528, 1050);
+	private var minRectPortraitGame:Rectangle = new Rectangle(568,0, 920, 1080);
+	private function getTargetRect():Rectangle
+	{
+		var ret:Rectangle = new Rectangle(0,0,0,0);
+		var iswide:Bool = size.width > size.height;
+		var mr:Rectangle =  iswide ? minRectGame : minRectPortraitGame;
 		
-		var s:Float = Math.max(size.width / bg.width, size.height / bg.height);
-		this.scale.x = this.scale.y = s;
+		var s:Float = Math.min(size.width / mr.width, size.height / mr.height);
+		ret.width = bg.width * s;
+		ret.height = bg.height * s;
 		
-		//center the select.
-		this.x = Math.round(( size.width - bg.width * s) / 2);
-		this.y = Math.round(( size.height - bg.height * s) / 2 );
-		
-		if (size.width > size.height)
+		ret.x = Math.round((size.width - bg.width * s)/2);
+		ret.y = Math.floor( Math.max( -mr.y * s, Math.floor(( size.height - this.bg.height * s+50*s)))); 
+		if (size.width < size.height)
 		{
-			this.y = 0;
+			//Limit bottom on mobile.
+			ret.y =Math.floor( Math.max( ret.y, (-1970*s+size.height)));
 		}
+		
+		return ret;
 	}
 }
